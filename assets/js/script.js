cef96c8783b03f9be09412fd54c837ddf118307f
Claire Sky <eclconnolly@gmail.com>
2021-11-06 22:11:29 -0700

    save to local storage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,58 @@
+var schedule = {};
+
+// function to save to local storage
+$(".saveBtn").on("click", function() {
+    var hour = $(this).parent().attr("id");
+    var text = $(this).siblings(".description").val();
+    schedule.text = text;
+    schedule.hour = hour;
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+});
+
+// function to retrieve and load schedule from local storage on page load
+var loadSchedule = function() {
+    schedule = JSON.parse(localStorage.getItem("schedule"));
+
+    $.each(schedule, function(list, arr) {
+        
+    });
+};
+
 // header date
 function updateTime() {
     var headerDay = moment().format('dddd, MMMM Do');
-    $("#currentDay").append(headerDay);
+    $("#currentDay").empty().append(headerDay);
 };
 
-// time refresh every 30 minutes
+// schedule color
+var colorSchedule = function() {
+    // get current time
+    var currentHour = moment().format("k");
+    for (i = 8; i < 17; i++) {
+        // remove any old classes from time blocks
+        $(".schedule-" + i).removeClass("past present future");
+        
+        // apply new class to time blocks
+        // past time block
+        if (currentHour > i) {
+        $(".schedule-" + i).addClass("past");
+        // current time block
+        } else if (currentHour == i) {
+            $(".schedule-" + i).addClass("present");
+        //future time block
+        } else {
+            $(".schedule-" + i).addClass("future");
+        };
+    };  
+};
+
+// time refresh every hour
 setInterval(function() {
     updateTime();
-}, 1000 * 6 * 30);
+}, 1000 * 60 * 60);
+
+setInterval(function() {
+    colorSchedule();
+}, 1000);
+
+updateTime();
