65c7987f476d8a4d703439160ff065b9d467a099
Claire Sky <eclconnolly@gmail.com>
2021-11-07 13:16:05 -0800

    retrieve from local storage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,26 @@
-var schedule = {};
-
 // function to save to local storage
-$(".saveBtn").on("click", function() {
+$(".time-block").on("click", ".saveBtn", function() {
     var hour = $(this).parent().attr("id");
     var text = $(this).siblings(".description").val();
-    schedule.text = text;
-    schedule.hour = hour;
-    localStorage.setItem("schedule", JSON.stringify(schedule));
+    // schedule[hour] = text
+    // localStorage.setItem("schedule", JSON.stringify(schedule));
+    localStorage.setItem(hour, text);
 });
 
-// function to retrieve and load schedule from local storage on page load
+// function to load schedule from local storage on page load
 var loadSchedule = function() {
-    schedule = JSON.parse(localStorage.getItem("schedule"));
+    for (i = 8; i <= 17; i++) {
+        $(".schedule" + i)[0].value = retrieveText("schedule" + i);
+        console.log(retrieveText("schedule" + i));
+    };
+};
 
-    $.each(schedule, function(list, arr) {
-        
-    });
+// function to retrieve local storage
+var retrieveText = function(text) {
+    if (!localStorage.getItem(text)) {
+        return ""; 
+    }
+    return localStorage.getItem(text);
 };
 
 // header date
@@ -28,31 +33,30 @@ function updateTime() {
 var colorSchedule = function() {
     // get current time
     var currentHour = moment().format("k");
-    for (i = 8; i < 17; i++) {
+    for (i = 8; i <= 17; i++) {
         // remove any old classes from time blocks
-        $(".schedule-" + i).removeClass("past present future");
+        $(".schedule" + i).removeClass("past present future");
         
         // apply new class to time blocks
         // past time block
         if (currentHour > i) {
-        $(".schedule-" + i).addClass("past");
+        $(".schedule" + i).addClass("past");
         // current time block
         } else if (currentHour == i) {
-            $(".schedule-" + i).addClass("present");
+            $(".schedule" + i).addClass("present");
         //future time block
         } else {
-            $(".schedule-" + i).addClass("future");
+            $(".schedule" + i).addClass("future");
         };
     };  
 };
 
-// time refresh every hour
-setInterval(function() {
-    updateTime();
-}, 1000 * 60 * 60);
-
+// time refresh every second
 setInterval(function() {
     colorSchedule();
+    updateTime();
 }, 1000);
 
+colorSchedule();
 updateTime();
+loadSchedule();
